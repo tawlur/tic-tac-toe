16768fd587359264ff37be17fd64b064ee9e5394
Macbook Pro <macbookpro@Tawlur-MBP.hsd1.co.comcast.net>
2020-05-03 19:47:08 -0600

    Fixed more bugs proper functioning - turn complete

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
 //  constants
 
 const playerLookup = {
-    '1': 'purple',
-    '-1': 'lime',
+    '1': 'Player X',
+    '-1': 'Player O',
     'null': 'transparent'
 };
 /*----- app's state (variables) -----*/
 let board;  //Array of column arrays with 1, -1, or null
 let turn; // 1 or -1 player
-let winner; // 1 = player 1 // -1 = player 2 //  T = tie  // null = no winner/tie
+let winner; // 1 = player 1 // -1 = player 2 //  T = tie  // null = game in play/tie
 
 /*----- cached element references -----*/
 const markerEls = Array.from(document.querySelectorAll("#board > div"));
-
+// pair message for turn
+const msgEl = document.getElementById('msg');
+//const msgEl = Array.from(document.querySelector);
 
 /*----- event listeners -----*/
 document.getElementById('board')
@@ -34,13 +36,22 @@ function init() {
     render();
 }
 
+// Transfer state to the dom
 function render() {
     //render board
     board.forEach(function(colArr, colIdx) {
         colArr.forEach(function(cell, rowIdx) {
             const div = document.getElementById(`c${colIdx}r${rowIdx}`)
+            div.style.backgroundColor = playerLookup [cell];
         })
     })
+    // render turn or winner message
+    if (winner) {
+
+    } else {
+        //render whos turn
+        msgEl.innerHTML = `${playerLookup[turn]}'s Turn`;
+    }
 };
 
 function handleClick(evt) {
@@ -48,5 +59,11 @@ function handleClick(evt) {
     const colIdx = markerEls.indexOf(evt.target);
     // Click in the box ensure
     if (colIdx === -1) return;
-        console.log(colIdx)
+    //get inex of null in col array
+    const rowIdx = board
+
+    turn *= -1;
+    // turn = turn times -1
+    render();
+//console.log(colIdx);
 }

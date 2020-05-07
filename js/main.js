73c1c0693929c11a27bdb1e71196709770a1c550
Macbook Pro <macbookpro@Tawlur-MBP.hsd1.co.comcast.net>
2020-05-07 15:42:06 -0600

    Finalizing game

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 //  constants
 
 const playerLookup = {
-    '1': 'Player X',
-    '-1': 'Player O',
+    '1': '  X',
+    '-1': '  O',
     'null': null
 };
 /*----- app's state (variables) -----*/
@@ -65,3 +65,15 @@ function handleClick(evt) {
 console.log(colIdx);
 }
 
+
+// Minimum Requirements
+
+// Display an empty tic-tac-toe board when the page is initially displayed.
+
+// A player can click on the nine cells to make a move.
+
+// Every click will alternate between marking an X and O.
+
+// Once occupied with an X or O, the cell cannot be played again.
+
+// Provide a Reset Game button that will clear the contents of the board.
